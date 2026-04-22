bd1b4c143dfc2f25f5a4701312d4463568b8389f
Indistanced <aidank1045@gmail.com>
2026-04-22 22:11:52 +0100

    Fix syntax

--- combat.cpp
+++ combat.cpp
@@ -4,7 +4,7 @@
 //Changes made:
 //- Separate enemy and player attack cycles to enhance readability 
 //- Add in extra enemy attack strategies
-//- Add in random item drop change using rand() fucntion
+//- Add in random item drop change using rand() function
 //- Add in exit game option to combat sequence 
 
 
@@ -33,7 +33,7 @@ void Combat::playerMove(Player* p, Enemy*& e) {
 
 			if (choice < 1 || choice > 3 || std::cin.fail()) {
 				clearBuffer();
-				std::cout << "Invaild choice, try again.\n";
+				std::cout << "Invalid choice, try again.\n";
 			}
 		} while (choice < 1 && choice > 3);
 		
@@ -82,31 +82,31 @@ void Combat::playerMove(Player* p, Enemy*& e) {
 			break;
 		}
 		case 2: {
-			auto potions = p->getInventory().getPotions(); //get all potions from inventory
+			auto potions = p->getInventory().getPotions(); // Get all potions from inventory
 
-			if (potions.empty()) { //check to make sure player has potions
+			if (potions.empty()) { // Check to make sure player has potions
 				std::cout << "\nYou have no potions!\n";
 				break;
 			}
 
-			p->getInventory().displayPotions(); // display all available potions 
+			p->getInventory().displayPotions(); // Display all available potions 
 
 			int potionChoice;
-			//get the user to select a potion
+			// Get the user to select a potion
 			do {
 				clearBuffer();
 				std::cout << "Choose potion: ";
 				std::cin >> potionChoice;
 			} while (potionChoice < 1 || potionChoice > potions.size() || std::cin.fail());
 
-			Potion* po = potions[potionChoice - 1];  //gets the potion with the correct index
+			Potion* po = potions[potionChoice - 1];  // Get the potion with the correct index
 
-			//try to use potion 
+			// Try to use potion 
 			if (po->use_potion(p)) {
 				std::cout << "\nHealth: " << p->getHealth()
 					<< "/" << p->getMaxHealth() << "\n";
 
-				//if quantity drops to zero remove potion from inventory 
+				// If quantity drops to zero, remove potion from inventory 
 				if (po->get_quantity() == 0) {
 					p->getInventory().removeItem(po);
 				} 
@@ -116,12 +116,12 @@ void Combat::playerMove(Player* p, Enemy*& e) {
 			break;
 		}
 		case 3: {
-			exit(0); // quit program
+			exit(0); // Quit program
 		}
 		default: std::cout << "\nInvalid choice.\n";
 		}
 
-	} while (choice != 1); // loop until player attacks 
+	} while (choice != 1); // Loop until player attacks 
 }
 
 
@@ -133,13 +133,13 @@ void Combat::enemyMove(Player* p, Enemy*& e) {
 	int randNum = rand() % 3; // 0 to 2
 
 	if (randNum == 0) {
-		damage = e->getStrength() + (rand() % 10 + 5); // strong attack damage
+		damage = e->getStrength() + (rand() % 10 + 5); // Strong attack damage
 		std::cout << "The enemy attacks aggressively!\n";
 	}
 	else if (randNum == 1) {
-		damage = e->getStrength() + rand() % 5; // low attack damage
+		damage = e->getStrength() + rand() % 5; // Low attack damage
 		int heal = 1 + rand() % (e->getMaxHealth() / 10 + 1);
-		e->addHealth(heal); // gain health back
+		e->addHealth(heal); // Gain health back
 		std::cout << "The enemy plays defensively and recovers some health!\n";
 	}
 	else {
@@ -156,9 +156,9 @@ void Combat::enemyMove(Player* p, Enemy*& e) {
 
 bool Combat::startCombat(Player* p, std::string enemy_name, int health, int attack, std::string element) {
 
-	Enemy* e = new Enemy(enemy_name, health, attack, element);  // create enemy entity using stats
+	Enemy* e = new Enemy(enemy_name, health, attack, element);  // Create enemy entity using stats
 
-	while (p->isAlive() && e->isAlive()) {  //game loop while both entites are alive 
+	while (p->isAlive() && e->isAlive()) {  // Game loop while both entities are alive 
 
 		// PLAYER TURN
 
@@ -170,8 +170,8 @@ bool Combat::startCombat(Player* p, std::string enemy_name, int health, int atta
 		std::cout << "\nPlayer Health: " << p->getHealth() << "\n";
 		std::cout << e->getName() << " Health: " << e->getHealth() << "\n";
 
-		playerMove(p, e);  //let player attack
-		if (!e->isAlive()) break;  //check if enemy is still alive
+		playerMove(p, e);  // Let player attack
+		if (!e->isAlive()) break;  // Check if enemy is still alive
 		system("pause");
 
 		// ENEMY TURN
@@ -184,8 +184,8 @@ bool Combat::startCombat(Player* p, std::string enemy_name, int health, int atta
 		std::cout << "\nPlayer Health: " << p->getHealth() << "\n";
 		std::cout << e->getName() << " Health: " << e->getHealth() << "\n";
 
-		enemyMove(p, e); //enemy move 
-		if (!p->isAlive()) break; //check if player is avlive 
+		enemyMove(p, e); // Enemy move 
+		if (!p->isAlive()) break; // Check if player is alive 
 		system("pause");
 
 	}
@@ -202,11 +202,11 @@ bool Combat::startCombat(Player* p, std::string enemy_name, int health, int atta
 		std::cout << "\nCongratulations! You defeated the " << e->getName() << "!\n";
 		std::cout << "  ── You Gained +" << exp << " Experience! ──\n";
 
-		p->addExp(exp); //add experince points to player 
+		p->addExp(exp); // Add experience points to player 
 
 		std::cout << "\nThe " << e->getName() << " dropped:\n";
 		item_drop_generator(p, e->getMaxHealth());  //generate random items using enemy health
-		delete e; //delete enemey 
+		delete e; // Delete enemy 
 		return true;
 	}
 
@@ -217,7 +217,7 @@ bool Combat::startCombat(Player* p, std::string enemy_name, int health, int atta
 
 void Combat::item_drop_generator(Player* p, int enemy_health) {
 
-	//random number generators to randomise item drop generation 
+	// Pseudorandom number generators to randomise item drop generation 
 	int rand_num = rand() % 101; // 0 to 100
 	int rand_element = rand() % 3; // 0 to 2
 
@@ -245,7 +245,7 @@ void Combat::item_drop_generator(Player* p, int enemy_health) {
 		}
 	}
 
-	//medium teir drops
+	// Medium tier drops
 	else if (enemy_health <= 100) {
 		std::cout << "- Medium Health Potion\n";
 		p->getInventory().addPotion("Medium Health Potion", "Restores +50 HP", 50, 1);
@@ -269,14 +269,14 @@ void Combat::item_drop_generator(Player* p, int enemy_health) {
 		}
 	}
 
-	//upper teir drops 
+	// Upper-tier drops 
 	else if (enemy_health <= 200) {
 		std::cout << "- Large Health Potion\n";
 		p->getInventory().addPotion("Large Health Potion", "Restores +75 HP", 75, 1);
 
 		if (rand_num < 50) {
 			if (rand_element == 0) {
-				if (p->getInventory().addSpell("Hydro Connon", "Strong water ability", 31, 10, 40, "water")) {
+				if (p->getInventory().addSpell("Hydro Cannon", "Strong water ability", 31, 10, 40, "water")) {
 					std::cout << "- Hydro Cannon spell\n";
 				}
 			}
@@ -293,7 +293,7 @@ void Combat::item_drop_generator(Player* p, int enemy_health) {
 		}
 	}
 
-	//lengendary drops 
+	// Legendary drops 
 	else {
 		std::cout << "- Mega Health Potion\n";
 		p->getInventory().addPotion("Mega Health Potion", "Restores +100 HP", 100, 1);
